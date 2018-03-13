1e0e759e19f1de00847df6a52dd7b233c9be3ade
Talgat Amanbayev <amanbayev@gmail.com>
2018-03-13 11:09:32 +0600

    added fancy crap to admin panel that works

--- imports/ui/Empty.jsx
+++ imports/ui/Empty.jsx
@@ -1,30 +1,16 @@
 import React, { Component } from 'react'
 import { Line, Doughnut } from 'react-chartjs-2'
+import { withTracker } from 'meteor/react-meteor-data'
+import randomMC from 'random-material-color'
+import { TypesCollection } from '/imports/api/TypesCollection'
 
-function getRandomInt (min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+
+function getFields(input, field) {
+  let output = [];
+  for (let i=0; i < input.length ; ++i)
+      output.push(input[i][field]);
+  return output;
 }
-
-const getState = () => ({
-  labels: [
-    'Red',
-    'Green',
-    'Yellow'
-  ],
-  datasets: [{
-    data: [getRandomInt(50, 200), getRandomInt(100, 150), getRandomInt(150, 250)],
-    backgroundColor: [
-    '#CCC',
-    '#36A2EB',
-    '#FFCE56'
-    ],
-    hoverBackgroundColor: [
-    '#FF6384',
-    '#36A2EB',
-    '#FFCE56'
-    ]
-  }]
-});
 
 const data = {
   labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
@@ -53,87 +39,105 @@ const data = {
   ]
 };
 
-class Donut extends Component {
+class Empty extends Component {
   constructor(props) {
     super(props)
-    this.state = getState();
   }
-
-  componentWillMount() {
-    this.myInterval = setInterval(()=>{
-      this.setState(getState());
-    }, 5000);
+  getColorsList(){
+    let limit = this.props.labels.length
+    let colors = []
+    for (let i=0; i< limit; i++){
+      colors.push(randomMC.getColor())
+    }
+    // console.log('i got called')
+    // console.log(colors)
+    return colors
   }
-
-  componentWillUnmount(){
-    clearInterval(this.myInterval)
-  }
-
   render() {
-    return (
-      <div className="col-sm">
-        <h2>Tester</h2>
-        <Doughnut data={this.state} />
-      </div>
-    )
+    if (this.props.loading) {
+      return (
+      <div>Loading</div>
+      )
+    } else {
+      // console.log(this.props.labels)
+      // console.log(this.props.counters) 
+      return (
+        <div>
+          <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pb-2 mb-3 border-bottom">
+            <h1 className="h2">Админка</h1>
+          </div>
+          <div className="row">
+            <div className="col-sm">
+              <h4>Количество записей в разрезе типов</h4>
+              <Doughnut data={{
+                'labels': this.props.labels,
+                'datasets': [{
+                  data: this.props.counters,
+                  backgroundColor: this.getColorsList(),
+                  hoverBackgroundColor: this.getColorsList()
+                }]
+              }} />
+            </div>
+          </div>
+          <br />
+          <div className="row">
+            <div className="col-sm">
+              <h2>Line chart example</h2>
+              <Line data={data} />
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-sm">
+              <div className="card">
+                <div className="card-header">
+                  Записи
+                </div>
+                <div className="card-body">
+                  <h5>
+                    Количество записей в базе данных: #
+                  </h5>
+                  <p className="card-text">
+                    Сегодня добавлено: #<br/>
+                    Сегодня просмотрено: #<br/>
+                    Добавлено в избранное, всего: # <br/>
+                  </p>
+                  <a href="#" className="btn btn-primary">Просмотреть детали</a>
+                </div>
+              </div>
+            </div>
+            <div className="col-sm">
+              <div className="card">
+                <div className="card-header">
+                  Еще что-нибудь полезное
+                </div>
+                <div className="card-body">
+                  <h5>
+                    Количество записей в базе данных: #
+                  </h5>
+                  <p className="card-text">
+                    Сегодня добавлено: #<br/>
+                    Сегодня просмотрено: #<br/>
+                    Добавлено в избранное, всего: # <br/>
+                  </p>
+                  <a href="#" className="btn btn-primary">Просмотреть детали</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      )
+    }
   }
-}
+} 
 
-const Empty = () => (
-  <div>
-    <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pb-2 mb-3 border-bottom">
-      <h1 className="h2">Админка</h1>
-    </div>
-    <div className="row">
-      <Donut />
-      <Donut />
-    </div>
-    <br />
-    <div className="row">
-      <div className="col-sm">
-        <h2>Line chart example</h2>
-        <Line data={data} />
-      </div>
-    </div>
-    <div className="row">
-      <div className="col-sm">
-        <div className="card">
-          <div className="card-header">
-            Записи
-          </div>
-          <div className="card-body">
-            <h5>
-              Количество записей в базе данных: #
-            </h5>
-            <p className="card-text">
-              Сегодня добавлено: #<br/>
-              Сегодня просмотрено: #<br/>
-              Добавлено в избранное, всего: # <br/>
-            </p>
-            <a href="#" className="btn btn-primary">Просмотреть детали</a>
-          </div>
-        </div>
-      </div>
-      <div className="col-sm">
-        <div className="card">
-          <div className="card-header">
-            Еще что-нибудь полезное
-          </div>
-          <div className="card-body">
-            <h5>
-              Количество записей в базе данных: #
-            </h5>
-            <p className="card-text">
-              Сегодня добавлено: #<br/>
-              Сегодня просмотрено: #<br/>
-              Добавлено в избранное, всего: # <br/>
-            </p>
-            <a href="#" className="btn btn-primary">Просмотреть детали</a>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-)
-
-export default Empty
+export default withTracker(props=>{
+  let typesSub = Meteor.subscribe('types')
+  let typesArray = TypesCollection.find({}).fetch();
+  let ready = typesSub.ready()
+  return {
+    loading: !ready,
+    types: TypesCollection.find({}).fetch(),
+    labels: getFields(TypesCollection.find({}).fetch(), 'name'),
+    counters: getFields(TypesCollection.find({}).fetch(), 'counter')
+  }
+})(Empty)
